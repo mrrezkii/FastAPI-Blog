aebbf04892732126fa494a607be14f02316ff030
nando <muhammad.rezki.ananda@gmail.com>
2021-07-09 00:35:07 +0700

    Store blog to database

--- blog/main.py
+++ blog/main.py
@@ -1,13 +1,28 @@
-from fastapi import FastAPI
+from fastapi.param_functions import Body
+from blog.database import get_db
+from fastapi import FastAPI, Depends
 from . import schemas
 from . import models
-from .database import engine
+from .database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 
 models.Base.metadata.create_all(bind=engine)
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db 
+    finally:
+        db.close()
+
+
 app = FastAPI()
 
 @app.post('/blog')
-def create(request: schemas.Blog):
-    return request
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body = request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
