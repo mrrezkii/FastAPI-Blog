e1a3742ae52a8ba2db1918a9fc399bd677a207a5
nando <muhammad.rezki.ananda@gmail.com>
2021-07-09 09:18:50 +0700

    Delete & update a blog

--- blog/main.py
+++ blog/main.py
@@ -31,9 +31,22 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
 
 @app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def destroy(id, db: Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
+    blog.delete(synchronize_session=False)
     db.commit()
     return 'Done'
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id, request : schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
+    blog.update({'title' : request.title, 'body' : request.body})
+    db.commit()
+    return 'Done'
+
 
 
 @app.get('/blog')
